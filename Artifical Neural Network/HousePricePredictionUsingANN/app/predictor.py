b15e7afc12c9edc1b572956a5d23305081f9c84d
Milan Rai <milanraii014@gmail.com>
2026-09-12 19:03:50 +0545

    predictor

--- Artifical Neural Network/HousePricePredictionUsingANN/app/predictor.py
+++ Artifical Neural Network/HousePricePredictionUsingANN/app/predictor.py
@@ -1,4 +1,5 @@
-from model_loader import model
+from model_loader import model, columns, scaler
+import pandas as pd 
 
 def get_house_prediction(data):
     #Create Dictionary
@@ -16,5 +17,43 @@ def get_house_prediction(data):
     input_data['ocean_proximity_ISLAND'] = 0
     input_data['ocean_proximity_NEAR BAY'] = 0
     input_data['ocean_proximity_NEAR OCEAN'] = 0
-    
+    input_data['ocean_proximity_<1H OCEAN '] = 0
+
+
+    if data.ocean_proximity == 'INLAND':
+        input_data['ocean_proximity_INLAND'] = 1
+    elif data.ocean_proximity == 'ISLAND':
+        input_data['ocean_proximity_ISLAND'] = 1
+    elif data.ocean_proximity == 'NEAR BAY':
+        input_data['ocean_proximity_NEAR BAY'] = 1
+    elif data.ocean_proximity == 'NEAR OCEAN':
+        input_data['ocean_proximity_NEAR OCEAN'] = 1
+    elif data.ocean_proximity == "<1H OCEAN":
+        input_data['ocean_proximity_<1H OCEAN '] = 1
+    else:
+        raise ValueError( "Invalid ocean_proximity. " "Use: <1H OCEAN, INLAND, ISLAND, NEAR BAY, NEAR OCEAN" )
+
+    #Convet to dataframe
+    input_df = pd.DataFrame([input_data])
+
+    #Arrange the colims in traning order
+    input_df = input_df.reindex(
+        columns= columns,
+        fill_value=0
+    )
+    #Scale input 
+    input_scaled = scaler.tranform(input_df)
+
+    #predict
+    prediction = model.predict(
+        input_scaled,
+        verbose = 0
+    )
+    return float(prediction[0][0])
+
+
+
+
+
+
 
