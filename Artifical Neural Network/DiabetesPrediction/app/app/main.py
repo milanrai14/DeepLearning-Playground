f00c77d363cfba530ddcafed2deb5e77e4407d14
Milan Rai <milanraii014@gmail.com>
2026-09-13 18:57:01 +0545

    push

--- Artifical Neural Network/DiabetesPrediction/app/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI,  HTTPException
-
-app = FastAPI()
-
-@app.get('/')
-def home():
-    return{
-        "message": "Diabetes Prediction Using Artificial Neural Network (ANN)"
-    }
