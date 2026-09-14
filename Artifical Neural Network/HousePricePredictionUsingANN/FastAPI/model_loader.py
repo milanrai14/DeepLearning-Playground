81dae7fccc419dd641009348748ae4a851a044db
Milan Rai <milanraii014@gmail.com>
2026-09-14 09:09:57 +0545

    House Price Prediction Regression with Deep learning ANN

--- Artifical Neural Network/HousePricePredictionUsingANN/FastAPI/model_loader.py
+++
@@ -1,9 +0,0 @@
-import joblib
-from keras.losses import MeanSquaredError
-from keras.models import load_model
-
-# Inside your loader function or at the top level:
-model = load_model("model\model_ann.h5", custom_objects={'mse': MeanSquaredError()})
-#Load preprocessing objects 
-scaler = joblib.load("model/scaler.pkl")
-columns = joblib.load("model/colums.plk")
